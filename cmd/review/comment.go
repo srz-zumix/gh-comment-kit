45d128e38615e67d799d704dbd38ba97fd6d8a7b
srz_zumix <zumix.cpp@gmail.com>
2026-01-01 23:51:45 +0900

    up

--- cmd/review/comment.go
+++ cmd/review/comment.go
@@ -37,7 +37,7 @@ func NewCommentCmd() *cobra.Command {
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			target := args[0]
-			repository, err := parser.Repository(parser.RepositoryInput(repo))
+			repository, err := parser.Repository(parser.RepositoryInput(repo), parser.RepositoryFromURL(target))
 			if err != nil {
 				return fmt.Errorf("failed to resolve repository: %w", err)
 			}
--- cmd/root.go
+++ cmd/root.go
@@ -9,13 +9,24 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/srz-zumix/gh-comment-kit/version"
 	"github.com/srz-zumix/go-gh-extension/pkg/actions"
+	"github.com/srz-zumix/go-gh-extension/pkg/gh/guardrails"
+	"github.com/srz-zumix/go-gh-extension/pkg/logger"
+)
+
+var (
+	logLevel string
+	readOnly bool
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "gh-commentator",
+	Use:     "gh-comment-kit",
 	Short:   "A tool to manage GitHub comments",
 	Long:    `A tool for posting trackable comments and providing the latest updates.`,
 	Version: version.Version,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logger.SetLogLevel(logLevel)
+		guardrails.NewGuardrail(guardrails.ReadOnlyOption(readOnly))
+	},
 }
 
 func Execute() {
@@ -29,4 +40,6 @@ func init() {
 	if actions.IsRunsOn() {
 		rootCmd.SetErrPrefix(actions.GetErrorPrefix())
 	}
+	logger.AddCmdFlag(rootCmd, rootCmd.PersistentFlags(), &logLevel, "log-level", "L")
+	rootCmd.PersistentFlags().BoolVar(&readOnly, "read-only", false, "Run in read-only mode (prevent write operations)")
 }
